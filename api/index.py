ac149426223a8984f380af0b4310cb3026231b79
Miles Hilliard <skywiredvt@gmail.com>
2025-10-06 15:42:30 -0400

    remove config.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,29 @@
 from flask import Flask, request, render_template, session, redirect, url_for
 import redis
 import math
-from .config import load_secret_key, load_config
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def load_secret_key():
+    return os.getenv('SECRET_KEY')
+
+def load_config():
+    UI_HOST = os.getenv('UI_HOST', '0.0.0.0')  # Example for additional config variables
+    UI_PORT = os.getenv('UI_PORT', 8080)
+
+    HOST = os.getenv('REDIS_HOST', 'localhost')
+    PORT = os.getenv('REDIS_PORT', 6379)
+    SSL = os.getenv('REDIS_SSL', 'false').lower() == 'true'
+
+    return {
+        "UI_HOST": UI_HOST,
+        "UI_PORT": UI_PORT,
+        "REDIS_HOST": HOST,
+        "REDIS_PORT": PORT,
+        "REDIS_SSL": SSL
+    }
 
 app = Flask(__name__)
 app.secret_key = load_secret_key()
